250d97339dc126d550f925554862bae09d115c6b
glory1610 <yaroslava.harasymchuk@lnu.edu.ua>
2024-04-29 14:33:56 +0300

    add sort use cases for collection

---
+++ domain/src/main/java/com/example/domain/usecase/SortCollectionByDate.kt
@@ -0,0 +1,12 @@
+package com.example.domain.usecase
+
+import com.example.domain.models.Collection
+import com.example.domain.repository.CollectionRepository
+
+class SortCollectionsByDate(private val collectionRepository: CollectionRepository) {
+
+    suspend operator fun invoke(): List<Collection> {
+        val collections = collectionRepository.getAllCollections()
+        return collections.sortedBy { it.creationDate }
+    }
+}
