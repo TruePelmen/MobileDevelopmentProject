e1104c319692da90cf487d566192696d6434108e
TruePelmen <oleksii.senyshyn@lnu.edu.ua>
2024-05-29 17:04:22 +0300

    Navigation as separated file

--- app/src/main/java/com/example/smartnotetaker/MainActivityCompose.kt
+++ app/src/main/java/com/example/smartnotetaker/MainActivityCompose.kt
@@ -4,26 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Info
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.example.smartnotetaker.di.appModule
 import com.example.smartnotetaker.di.dataModule
 import com.example.smartnotetaker.di.domainModule
-import com.example.smartnotetaker.screens.AddCollectionScreen
-import com.example.smartnotetaker.screens.AddNoteScreen
-import com.example.smartnotetaker.screens.CollectionsScreen
-import com.example.smartnotetaker.screens.DeleteCollectionDialog
-import com.example.smartnotetaker.screens.EditCollectionScreen
-import com.example.smartnotetaker.screens.NotesScreen
-import com.example.smartnotetaker.ui.theme.SmartNoteTakerTheme
+import com.example.smartnotetaker.navigation.AppNavigation
 import org.koin.android.ext.android.get
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.GlobalContext.startKoin
@@ -40,56 +24,11 @@ class MainActivityCompose : ComponentActivity() {
         val viewModel: MainViewModel = get<MainViewModel>()
         enableEdgeToEdge()
         setContent {
-            val navController = rememberNavController()
-            SmartNoteTakerTheme {
-                Scaffold(modifier = Modifier.padding(0.dp)) { innerPadding ->
-                    NavHost(
-                        navController = navController,
-                        startDestination = "Collections",
-                        modifier = Modifier.padding(innerPadding)
-                    ) {
-                        composable("Collections") {
-                            CollectionsScreen(
-                                viewModel = viewModel,
-                                navController = navController,
-                                modifier = Modifier.fillMaxSize()
-                            )
-                        }
-                        composable("AddCollection") {
-                            AddCollectionScreen(navController = navController, viewModel = viewModel)
-                        }
-                        composable("EditCollection/{collectionId}") { backStackEntry ->
-                            val collectionId = backStackEntry.arguments?.getString("collectionId") ?: return@composable
-                            EditCollectionScreen(collectionId, navController, viewModel)
-                        }
-                        composable("DeleteCollectionDialog/{collectionId}"){backStackEntry ->
-                            val collectionId = backStackEntry.arguments?.getString("collectionId") ?: return@composable
-                            DeleteCollectionDialog(
-                                collectionId = collectionId,
-                                onDismissRequest = { navController.popBackStack() },
-                                onConfirmation = { viewModel.getCollectionById(collectionId)
-                                    ?.let { viewModel.deleteCollection(it)
-                                    viewModel.loadCollections()}
-                                    navController.popBackStack()},
-                                dialogTitle = "Deleting the collection",
-                                dialogText = "Are you sure you want to delete ${viewModel.getCollectionById(collectionId)}",
-                                icon = Icons.Default.Info
-                            )
-                        }
-                        composable("Notes/{collectionId}") { backStackEntry ->
-                            val collectionId = backStackEntry.arguments?.getString("collectionId") ?: return@composable
-                            NotesScreen(collectionId, navController, viewModel)
-                        }
-                        composable("AddNote/{collectionId}"){backStackEntry ->
-                            val collectionId = backStackEntry.arguments?.getString("collectionId") ?: return@composable
-                            AddNoteScreen(collectionId.toLong(), navController, viewModel)
-                        }
-                    }
-                }
-            }
+            AppNavigation(viewModel = viewModel)
         }
     }
 }
+
 
 
 
---
+++ app/src/main/java/com/example/smartnotetaker/navigation/AppNavigation.kt
@@ -0,0 +1,63 @@
+package com.example.smartnotetaker.navigation
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.smartnotetaker.MainViewModel
+import com.example.smartnotetaker.screens.AddCollectionScreen
+import com.example.smartnotetaker.screens.AddNoteScreen
+import com.example.smartnotetaker.screens.CollectionsScreen
+import com.example.smartnotetaker.screens.DeleteCollectionDialog
+import com.example.smartnotetaker.screens.EditCollectionScreen
+import com.example.smartnotetaker.screens.NotesScreen
+
+@Composable
+fun AppNavigation(modifier: Modifier = Modifier, viewModel: MainViewModel) {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = "Collections",
+    ) {
+        composable("Collections") {
+            CollectionsScreen(
+                viewModel = viewModel,
+                navController = navController,
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+        composable("AddCollection") {
+            AddCollectionScreen(navController = navController, viewModel = viewModel)
+        }
+        composable("EditCollection/{collectionId}") { backStackEntry ->
+            val collectionId = backStackEntry.arguments?.getString("collectionId") ?: return@composable
+            EditCollectionScreen(collectionId, navController, viewModel)
+        }
+        composable("DeleteCollectionDialog/{collectionId}"){backStackEntry ->
+            val collectionId = backStackEntry.arguments?.getString("collectionId") ?: return@composable
+            DeleteCollectionDialog(
+                collectionId = collectionId,
+                onDismissRequest = { navController.popBackStack() },
+                onConfirmation = { viewModel.getCollectionById(collectionId)
+                    ?.let { viewModel.deleteCollection(it)
+                        viewModel.loadCollections()}
+                    navController.popBackStack()},
+                dialogTitle = "Deleting the collection",
+                dialogText = "Are you sure you want to delete ${viewModel.getCollectionById(collectionId)}",
+                icon = Icons.Default.Info
+            )
+        }
+        composable("Notes/{collectionId}") { backStackEntry ->
+            val collectionId = backStackEntry.arguments?.getString("collectionId") ?: return@composable
+            NotesScreen(collectionId, navController, viewModel)
+        }
+        composable("AddNote/{collectionId}"){backStackEntry ->
+            val collectionId = backStackEntry.arguments?.getString("collectionId") ?: return@composable
+            AddNoteScreen(collectionId.toLong(), navController, viewModel)
+        }
+    }
+}
